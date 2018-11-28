8a5d3c79682f0eb85f695f1cccd963be3312bbe7
Mauricio Neira <m.neira10@uniandes.edu.co>
2018-11-27 19:46:41 -0500

    1st stage selection

---
+++ proyectoFinal/simulations/1stLevel/simulate.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+double const gamman = 1.0/30.0;
+
+
+using namespace std;
+
+main(int argc, char const *argv[])
+{
+  cout<<"main check";
+  /* code */
+  return 0;
+}
